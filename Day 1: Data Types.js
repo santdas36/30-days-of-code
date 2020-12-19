47fb1b4b7bd0ff88b3591cef13b39209ce43a9b8
santdas36 <santdas36@gmail.com>
2020-12-19 13:21:44 +0530

    day 1

---
+++ Day 1: Data Types.js
@@ -0,0 +1,11 @@
+function main() {
+  var i = 4;
+  var d = 4.0;
+  var s = "HackerRank ";
+  let i1 = readLine();
+  let d1 = readLine();
+  let s1 = readLine();
+  console.log(parseInt(i) + parseInt(i1));
+  console.log((parseFloat(d) + parseFloat(d1)).toFixed(1));
+  console.log(s + s1);
+}
